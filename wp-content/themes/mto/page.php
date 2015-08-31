f5ac516c6fd09a11832405d8ce1a96de262faef1
Iaan van Niekerk <iaan@plusplusminus.co.za>
2015-08-31 17:38:31 +0200

    more design

--- wp-content/themes/mto/page.php
+++ wp-content/themes/mto/page.php
@@ -1,19 +1,31 @@
 <?php get_header(); ?>
 
-	<div class="single_page">
-		<div class="container">
+<div class="single_page">
+	<div class="container">
 		<div class="row">
 			<div class="col-md-8">
-				<article class="page_article">
-					<h1 class="page_article--title"><?php the_title(); ?></h1>
-					<div class="article_entry">
-						<?php the_content(); ?>
-					</div>
-				</article>
-			</div>
-			<?php get_sidebar(); ?>
-		</div>
+				<?php if ( have_posts() ) : ?>
 
+					<?php while ( have_posts() ) : the_post(); ?>
+				
+					<article class="page_article">
+						<h1 class="page_article--title"><?php the_title(); ?></h1>
+					
+						<div class="article_entry">
+							<?php the_content(); ?>
+						</div>
+					</article>
+
+					<?php endwhile; ?>
+				
+				<?php endif; ?>
+
+			<?php wp_reset_query(); ?>
+
+			</div>
+		<?php get_sidebar(); ?>
+		</div>
 	</div>
+</div>
 
 <?php get_footer(); ?>
